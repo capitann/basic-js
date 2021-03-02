3ba25dfe3f6f1e5e7d6a8b6dc24e0cdf11cb0a8a
Hanna <ann.sadovskaya@gmail.com>
2021-03-02 18:09:43 +0100

    feat: resolve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof (position) !== 'number' || position - 1 < 0 || position - 1 >= this.getLength()) {
+      this.chain = [];
+      throw new Error();
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let fullChain = '';
+    for (let i = 0; i < this.getLength(); i++) {
+      fullChain += `( ${this.chain[i]} )`;
+      if (i < this.getLength() - 1) {
+        fullChain += '~~';
+      }
+    }
+    this.chain = [];
+    return fullChain;
   }
 };
 
