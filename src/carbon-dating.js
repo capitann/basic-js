5a083eaa951840369eaa99535202cf656505aab6
Hanna <ann.sadovskaya@gmail.com>
2021-03-02 18:06:29 +0100

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (sampleActivity === null || isNaN(parseFloat(sampleActivity)) || typeof(sampleActivity) !== 'string' || parseFloat(sampleActivity) > MODERN_ACTIVITY || parseFloat(sampleActivity) <= 0){
+   return false;
+ }
+ return Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / 0.693 * HALF_LIFE_PERIOD);
 };
