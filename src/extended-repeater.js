da59ed24f1d9ff9a5442b19a19aa886cfed86444
asadovskaya <asadovskaya@twiced.de>
2021-03-02 18:48:19 +0100

    feature: check income param

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,28 +2,20 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
     let resultStr = '';
-    if ("repeatTimes" in options){
-        if (options.repeatTimes === undefined){
-            options.repeatTimes = 1;
-        }
-        for (let i = 0; i < options.repeatTimes; i++) {
+    let repeatTimes = "repeatTimes" in options ? options.repeatTimes : 1;
+        for (let i = 0; i < repeatTimes; i++) {
             resultStr += `${str}`;
-            if ("additionRepeatTimes" in options) {
-                if (options.additionRepeatTimes === undefined){
-                    options.additionRepeatTimes = 1;
-                }
-                for (let j = 0; j < options.additionRepeatTimes; j++) {
+            let additionRepeatTimes = "additionRepeatTimes" in options ? options.additionRepeatTimes : 1;
+                for (let j = 0; j < additionRepeatTimes; j++) {
                     resultStr += `${"addition" in options ? options.addition : ''}`;
-                    if (j != options.additionRepeatTimes - 1) {
+                    if (j != additionRepeatTimes - 1) {
                         resultStr += `${("additionSeparator" in options) ? options.additionSeparator : '|'}`;
                     }
                 }
-            }
-            if (i != options.repeatTimes - 1) {
+            if (i != repeatTimes - 1) {
                 resultStr += `${"separator" in options ? options.separator : '+'}`;
             }
         }
-    }
     return resultStr;
 };
   
