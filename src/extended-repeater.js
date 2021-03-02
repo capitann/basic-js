0e29a1bee97736714add8e14caaad565e0f41989
Hanna <ann.sadovskaya@gmail.com>
2021-03-02 18:08:06 +0100

    feat: resolve extended-repeat

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let resultStr = '';
+    if ("repeatTimes" in options){
+        if (options.repeatTimes === undefined){
+            options.repeatTimes = 1;
+        }
+        for (let i = 0; i < options.repeatTimes; i++) {
+            resultStr += `${str}`;
+            if ("additionRepeatTimes" in options) {
+                if (options.additionRepeatTimes === undefined){
+                    options.additionRepeatTimes = 1;
+                }
+                for (let j = 0; j < options.additionRepeatTimes; j++) {
+                    resultStr += `${"addition" in options ? options.addition : ''}`;
+                    if (j != options.additionRepeatTimes - 1) {
+                        resultStr += `${("additionSeparator" in options) ? options.additionSeparator : '|'}`;
+                    }
+                }
+            }
+            if (i != options.repeatTimes - 1) {
+                resultStr += `${"separator" in options ? options.separator : '+'}`;
+            }
+        }
+    }
+    return resultStr;
 };
   
