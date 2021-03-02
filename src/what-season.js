5d2663ce7cd8273450d45a7d84cbcd95ac57438f
Hanna <ann.sadovskaya@gmail.com>
2021-03-02 18:11:46 +0100

    feat: add what-season desicion

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date){
+    return "Unable to determine the time of year!";
+  }
+  if (!date instanceof Date || isNaN(date)){
+    throw new Error();
+  }
+  let month = date.getMonth() + 1;
+  if ([12, 1, 2].includes(month)){
+    return 'winter';
+  }
+  if ([3, 4, 5].includes(month)){
+    return 'spring';
+  }
+  if ([6, 7, 8].includes(month)){
+    return 'summer';
+  }
+  if ([9, 10, 11].includes(month)){
+    return 'autumn';
+  }
+  return 'winter';
 };
