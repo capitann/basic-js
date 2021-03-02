e090f1c0e063a202a1ebd2538a93a9c88fc2cea7
Hanna <ann.sadovskaya@gmail.com>
2021-03-02 18:12:05 +0100

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
   if (sampleActivity === null || isNaN(parseFloat(sampleActivity)) || typeof(sampleActivity) !== 'string' || parseFloat(sampleActivity) > MODERN_ACTIVITY || parseFloat(sampleActivity) <= 0){
    return false;
  }
